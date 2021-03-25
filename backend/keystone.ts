e39a194d0df415bb98a69b4f6811efa4d8e307de
Fabio <fabioac71@gmail.com>
2021-03-25 10:22:17 -0300

    first commit

---
+++ backend/keystone.ts
@@ -0,0 +1,63 @@
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  statelessSessions,
+  withItemData,
+} from '@keystone-next/keystone/session';
+import { createAuth } from '@keystone-next/auth';
+import { User } from './schemas/User';
+import { Product } from './schemas/Product';
+import { ProductImage } from './schemas/ProductImage';
+import 'dotenv/config';
+
+let sessionSecret = process.env.SESSION_SECRET;
+
+if (!sessionSecret) {
+  if (process.env.NODE_ENV === 'production') {
+    throw new Error(
+      'The SESSION_SECRET environment variable must be set in production'
+    );
+  } else {
+    sessionSecret = process.env.COOKIE_SECRET;
+  }
+}
+
+let sessionMaxAge = 60 * 60 * 24 * 30; // 30 days
+
+const auth = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+  },
+});
+
+export default auth.withAuth(
+  config({
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true
+      }
+    },
+    db: {
+      adapter: "mongoose",
+      url: process.env.DATABASE_URL,
+    },
+    ui: {
+      isAccessAllowed: (context) => !!context.session?.data,
+    },
+    lists: createSchema({
+      User,
+      Product,
+      ProductImage,
+    }),
+    session: withItemData(
+      statelessSessions({
+        maxAge: sessionMaxAge,
+        secret: sessionSecret,
+      }),
+      { User: 'name' }
+    ),
+  })
+);
